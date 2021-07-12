539939911806f7aa6fabc51c4c2f57e83be6e7b8
masaki.mizuno <masaki.mizuno@proaxia-consulting.co.jp>
2021-07-12 11:01:58 +0900

    20210712_1101

---
+++ GitHubActionsStudy2/GitHubActionsStudy2/GitHubActionsStudy2App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubActionsStudy2App.swift
+//  GitHubActionsStudy2
+//
+//  Created by MASAKI MIZUNO on 2021/07/12.
+//
+
+import SwiftUI
+
+@main
+struct GitHubActionsStudy2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
